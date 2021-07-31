c20264b54b50bc86ae7d84c823552d1d82e19f3f
Sergio Bugallo <DnaMaster.22@gmail.com>
2021-07-31 15:24:27 +0200

    Add Conan recipe (#21)
    * Add conanfile
    
    * Update README.md
    
    * Add missing step in README.md
    
    * Add options to conan recipe
    
    * Fix option checking in conanfile when value is None
    
    * Update README

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ https://github.com/grimoire/mmdetection-to-tensorrt
 
 - Install tensorrt7: https://developer.nvidia.com/tensorrt
 
+### From sources
+
 clone the repo and create build folder
 
 ```shell
@@ -50,3 +52,71 @@ set the envoirment variable(in ~/.bashrc):
 export AMIRSTAN_LIBRARY_PATH=<amirstan_plugin_root>/build/lib
 ```
 
+### Using Conan
+
+- Install [Conan](https://conan.io/): 
+
+```bash
+pip install conan
+```
+
+- Register grimoire's Conan remote:
+
+```bash
+conan remote add grimoire https://grimoire.jfrog.io/artifactory/api/conan/grimoire-conan
+```
+
+- Add a `conanfile.txt` file to your project's root with the following content:
+
+```
+[requires]
+amirstan_plugin/0.4.1
+
+[generators]
+cmake
+```
+
+- Additionaly, you can add a few options under the \[options\] section to configure your build:
+
+  * tensorrt_dir: path where TensorRT is located. Default `~/SDK/TensorRT`.
+  * with_deepstream: whether to compile with deepstream support. Default `False`.
+  * deepstream_dir: path where deepstream is located. Default `/opt/nvidia/deepstream/deepstream`
+  * cub_root_dir: Default `./third_party/cub`
+  * cuda_arch: list of CUDA architectures to compile for. Default `61;62;70;72;75;80;86`
+
+  For example, to use a custom TensorRT dir and compile for a specific CUDA architecture:
+
+  ```
+  [requires]
+  amirstan_plugin/0.4.1
+
+  [generators]
+  cmake
+
+  [options]
+  amirstan_plugin:tensorrt_dir=/usr/include/x86_64-linux-gnu
+  amirstan_plugin:cuda_arch=75
+  ```
+
+- Add the following lines to your project root's `CMakeLists.txt`:
+
+```
+INCLUDE(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+CONAN_BASIC_SETUP()
+```
+
+- Add conan libs to the linking stage:
+
+```
+target_link_libraries(trt_sample PUBLIC ${CONAN_LIBS} ${CUDA_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT} ${TensorRT_LIBRARIES})
+```
+
+- Compile your project:
+
+```
+$ mkdir build
+$ cd build
+$ conan install .. -s compiler.libcxx=libstdc++11 --build=missing 
+$ cmake .. 
+$ make -j${nproc}
+```
---
+++ conanfile.py
@@ -0,0 +1,64 @@
+from conans import ConanFile, CMake
+
+
+class AmirstanPluginConan(ConanFile):
+    name = "amirstan_plugin"
+    version = "0.4.1"
+    license = "MIT"
+
+    url = "https://github.com/grimoire/amirstan_plugin.git"
+    repo_url = "https://github.com/grimoire/amirstan_plugin.git"
+    description = "Amirstan plugin contain some useful tensorrt plugin."
+    topics = ("tensorrt", "mmdetection")
+    settings = "os", "compiler", "build_type", "arch"
+    options = { 
+        "shared": [True, False],
+        "tensorrt_dir": "ANY",
+        "with_deepstream": [True, False],
+        "deepstream_dir": "ANY",
+        "cub_root_dir": "ANY",
+        "cuda_arch": "ANY"
+    }
+    default_options = { 
+        "shared": True,
+        "tensorrt_dir": None,
+        "with_deepstream": False,
+        "deepstream_dir": None,
+        "cub_root_dir": None,
+        "cuda_arch": "61;62;70;72;75;80;86"
+    }
+    generators = "cmake"
+    exports_sources = "src*", "include*", "lib*", "CMakeLists.txt", "cmake*"
+
+    def configure(self):
+        self.settings.compiler.libcxx = "libstdc++11"
+
+    def build(self):
+        cmake = CMake(self)
+
+        cmake.definitions["WITH_DEEPSTREAM"] = self.options.with_deepstream.value
+        cmake.definitions['GPU_ARCHS'] = self.options.cuda_arch.value
+
+        if self.options.tensorrt_dir.value != "None":
+            cmake.definitions["TENSORRT_DIR"] = self.options.tensorrt_dir.value
+
+        if self.options.deepstream_dir != "None" and self.options.with_deepstream.value:
+            cmake.definitions["DeepStream_DIR"] = self.options.deepstream_dir.value
+        
+        if self.options.cub_root_dir != "None":
+            cmake.definitions["CUB_ROOT_DIR"] = self.options.cub_root_dir.value
+
+        cmake.configure(source_folder=".")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", dst="include/plugin", src="include/plugin")
+        self.copy("*.h", dst="include/amir_cuda_util", src="include/amir_cuda_util")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so*", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+    
+    def package_info(self):
+        self.cpp_info.libs = ["amirstan_plugin"]
