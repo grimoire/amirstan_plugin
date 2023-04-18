f04b0fc8c78f13520f2134e1713a3432f955792a
Xabier Arauzo <xarauzo@gradiant.org>
2023-04-18 10:40:38 +0200

    modified bbox order

--- src/plugin/NvDsInferParseMmdet.cpp
+++ src/plugin/NvDsInferParseMmdet.cpp
@@ -14,7 +14,8 @@ extern "C" bool NvDsInferParseMmdet(
     std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
     NvDsInferNetworkInfo const &networkInfo,
     NvDsInferParseDetectionParams const &detectionParams,
-    std::vector<NvDsInferParseObjectInfo> &objectList) {
+    std::vector<NvDsInferParseObjectInfo> &objectList)
+{
   static int numDetectionLayerIndex = -1;
   static int boxesLayerIndex = -1;
   static int scoresLayerIndex = -1;
@@ -23,14 +24,18 @@ extern "C" bool NvDsInferParseMmdet(
   int numDetsToParse;
 
   /* Find the num_detections layer */
-  if (numDetectionLayerIndex == -1) {
-    for (unsigned int i = 0; i < outputLayersInfo.size(); i++) {
-      if (strcmp(outputLayersInfo[i].layerName, "num_detections") == 0) {
+  if (numDetectionLayerIndex == -1)
+  {
+    for (unsigned int i = 0; i < outputLayersInfo.size(); i++)
+    {
+      if (strcmp(outputLayersInfo[i].layerName, "num_detections") == 0)
+      {
         numDetectionLayerIndex = i;
         break;
       }
     }
-    if (numDetectionLayerIndex == -1) {
+    if (numDetectionLayerIndex == -1)
+    {
       std::cerr << "Could not find num_detection layer buffer while parsing"
                 << std::endl;
       return false;
@@ -38,14 +43,18 @@ extern "C" bool NvDsInferParseMmdet(
   }
 
   /* Find the boxes layer */
-  if (boxesLayerIndex == -1) {
-    for (unsigned int i = 0; i < outputLayersInfo.size(); i++) {
-      if (strcmp(outputLayersInfo[i].layerName, "boxes") == 0) {
+  if (boxesLayerIndex == -1)
+  {
+    for (unsigned int i = 0; i < outputLayersInfo.size(); i++)
+    {
+      if (strcmp(outputLayersInfo[i].layerName, "boxes") == 0)
+      {
         boxesLayerIndex = i;
         break;
       }
     }
-    if (boxesLayerIndex == -1) {
+    if (boxesLayerIndex == -1)
+    {
       std::cerr << "Could not find boxes layer buffer while parsing"
                 << std::endl;
       return false;
@@ -53,15 +62,19 @@ extern "C" bool NvDsInferParseMmdet(
   }
 
   /* Find the scores layer */
-  if (scoresLayerIndex == -1) {
-    for (unsigned int i = 0; i < outputLayersInfo.size(); i++) {
-      if (strcmp(outputLayersInfo[i].layerName, "scores") == 0) {
+  if (scoresLayerIndex == -1)
+  {
+    for (unsigned int i = 0; i < outputLayersInfo.size(); i++)
+    {
+      if (strcmp(outputLayersInfo[i].layerName, "scores") == 0)
+      {
         scoresLayerIndex = i;
         getDimsCHWFromDims(scoresLayerDims, outputLayersInfo[i].dims);
         break;
       }
     }
-    if (scoresLayerIndex == -1) {
+    if (scoresLayerIndex == -1)
+    {
       std::cerr << "Could not find scores layer buffer while parsing"
                 << std::endl;
       return false;
@@ -69,14 +82,18 @@ extern "C" bool NvDsInferParseMmdet(
   }
 
   /* Find the classes layer */
-  if (classesLayerIndex == -1) {
-    for (unsigned int i = 0; i < outputLayersInfo.size(); i++) {
-      if (strcmp(outputLayersInfo[i].layerName, "classes") == 0) {
+  if (classesLayerIndex == -1)
+  {
+    for (unsigned int i = 0; i < outputLayersInfo.size(); i++)
+    {
+      if (strcmp(outputLayersInfo[i].layerName, "classes") == 0)
+      {
         classesLayerIndex = i;
         break;
       }
     }
-    if (classesLayerIndex == -1) {
+    if (classesLayerIndex == -1)
+    {
       std::cerr << "Could not find classes layer buffer while parsing"
                 << std::endl;
       return false;
@@ -88,7 +105,8 @@ extern "C" bool NvDsInferParseMmdet(
   float *scores = (float *)outputLayersInfo[scoresLayerIndex].buffer;
   numDetsToParse = *(int *)(outputLayersInfo[numDetectionLayerIndex].buffer);
 
-  for (int indx = 0; indx < numDetsToParse; indx++) {
+  for (int indx = 0; indx < numDetsToParse; indx++)
+  {
     float outputX1 = bboxes[indx * 4];
     float outputY1 = bboxes[indx * 4 + 1];
     float outputX2 = bboxes[indx * 4 + 2];
@@ -98,16 +116,17 @@ extern "C" bool NvDsInferParseMmdet(
     float threshold =
         this_class >= 0 ? detectionParams.perClassThreshold[this_class] : 1.;
 
-    if (this_score >= threshold) {
+    if (this_score >= threshold)
+    {
       NvDsInferParseObjectInfo object;
 
-      object.classId = this_class;
-      object.detectionConfidence = this_score;
-
       object.left = outputX1;
-      object.top = outputY1;
       object.width = outputX2 - outputX1;
+      object.top = outputY1;
       object.height = outputY2 - outputY1;
+
+      object.detectionConfidence = this_score;
+      object.classId = this_class;
 
       objectList.push_back(object);
     }
